43163675866093a8ab86056e03aa726661389d48
Daniel Hooper <daniel@danielhooper.ca>
2015-07-06 11:14:16 -0400

    We needed to set up our location manager cocoapod and initialize a git repository.

---
+++ Photopography/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photopography
+//
+//  Created by Daniel Hooper on 2015-07-06.
+//  Copyright (c) 2015 Daniel Hooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Photopography/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Photopography
+//
+//  Created by Daniel Hooper on 2015-07-06.
+//  Copyright (c) 2015 Daniel Hooper. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
