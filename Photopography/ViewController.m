d9a3a0337f47517250561d3a1bb54d5499df80bb
Daniel Hooper <daniel@danielhooper.ca>
2015-07-06 12:12:07 -0400

    We needed to get a dummy location request working so we can build on working code.

--- Photopography/ViewController.m
+++ Photopography/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <INTULocationManager/INTULocationManager.h>
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property (weak, nonatomic) IBOutlet UIButton *requestCurrentLocationButton;
+
+@property (assign, nonatomic) INTULocationRequestID locationRequestID;
 
 @end
 
@@ -24,4 +30,37 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)startSingleLocationRequest
+{
+    __weak __typeof(self) weakSelf = self;
+    INTULocationManager *locMgr = [INTULocationManager sharedInstance];
+    self.locationRequestID = [locMgr requestLocationWithDesiredAccuracy:INTULocationAccuracyBlock
+                                                                timeout:10
+                                                                  block:
+                              ^(CLLocation *currentLocation, INTULocationAccuracy achievedAccuracy, INTULocationStatus status) {
+                                  __typeof(weakSelf) strongSelf = weakSelf;
+                                  
+                                  if (status == INTULocationStatusSuccess) {
+                                      // achievedAccuracy is at least the desired accuracy (potentially better)
+                                      strongSelf.statusLabel.text = [NSString stringWithFormat:@"Location request successful! Current Location:\n%@", currentLocation];
+                                  }
+                                  else if (status == INTULocationStatusTimedOut) {
+                                      // You may wish to inspect achievedAccuracy here to see if it is acceptable, if you plan to use currentLocation
+                                      strongSelf.statusLabel.text = [NSString stringWithFormat:@"Location request timed out. Current Location:\n%@", currentLocation];
+                                  }
+                                  else {
+                                      // An error occurred
+//                                      strongSelf.statusLabel.text = [strongSelf getErrorDescription:status];
+                                  }
+                                  
+//                                  strongSelf.locationRequestID = NSNotFound;
+                              }];
+}
+
+- (IBAction)startButtonTapped:(id)sender {
+    [self startSingleLocationRequest];
+}
+
+
 @end
+
